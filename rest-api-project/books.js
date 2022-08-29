1606aeda809ffb34fa7aa312fc54a45929e39447
Janice <98136740+jsmontecillo@users.noreply.github.com>
2022-08-29 14:51:14 -0700

    initial REST API commit

---
+++ rest-api-project/books.js
@@ -0,0 +1,69 @@
+const BOOKS = [{
+    "isbn": "0553120441",
+    "title": "Kafka on the Shore",
+    "author": "Haruki Murakami",
+    "format": "Paperback"
+},
+{
+    "isbn": "0679889175",
+    "title": "Communion: The Female Search for Love",
+    "author": "bell hooks",
+    "format": "Paperback"
+},
+{
+    "isbn": "9780857660282",
+    "title": "A Room of One's Own",
+    "author": "Virginia Woolf",
+    "format": "Paperback"
+},
+{
+    "isbn": "0553096095",
+    "title": "Breasts and Eggs",
+    "author": "Mieko Kawakami",
+    "format": "Hardcover"
+},
+{
+    "isbn": "05530962356",
+    "title": "Infinite Jest",
+    "author": "David Foster Wallace",
+    "format": "Hardcover"
+},
+{
+    "isbn": "0553098976",
+    "title": "Capitalist Realism",
+    "author": "Mark Fisher",
+    "format": "Hardcover"
+},
+{
+    "isbn": "0553093476",
+    "title": "The Bell Jar",
+    "author": "Sylvia Plath",
+    "format": "Hardcover"
+},
+{
+    "isbn": "0553115634",
+    "title": "Glitch Feminism",
+    "author": "Legacy Russell",
+    "format": "Hardcover"
+},
+{
+    "isbn": "0553115765",
+    "title": "Kitchen",
+    "author": "Banana Yoshimoto",
+    "format": "Hardcover"
+},
+{
+    "isbn": "05531324567",
+    "title": "1Q84",
+    "author": "Haruki Murakami",
+    "format": "Hardcover"
+},
+{
+    "isbn": "05531324511",
+    "title": "Kindly Bent to Ease Us: Mind",
+    "author": "Longchenpa",
+    "format": "Hardcover"
+}
+]
+
+ export default BOOKS;
