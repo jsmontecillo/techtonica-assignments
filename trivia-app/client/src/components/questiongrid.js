bf794cbde28ff2f8e9253e5cc4ec2cd60d98a35d
jsmontecillo <jsmontecillo@gmail.com>
2022-09-15 22:32:37 -0700

    updated score state

--- trivia-app/client/src/components/questiongrid.js
+++ trivia-app/client/src/components/questiongrid.js
@@ -1,19 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import "./questiongrid.css";
-import QuestionCard from "./questioncard"
-import audio from "./wii.mp3"
+import QuestionCard from "./questioncard";
+import audio from "./wii.mp3";
+import snoop from "./snoop.gif";
+import {decode} from 'html-entities';
 
 const QuestionGrid = () => {
     const [questionData, setQuestions] = useState(null);
     const [toggleQuestion, changeQuestion] = useState(0);
-    const [topicData, changeTopic] = useState("Art");
+    const [topicData, changeTopic] = useState("general");
+    const score = useRef(0);
+    const target = useRef();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         new Audio(audio).play();
-        fetch("http://localhost:4040/api/trivia")
+
+        fetch(`http://localhost:4040/api/trivia/${topicData}`)
         .then((response) => {
             if(response.ok) {
+                decode('&lt; &gt; &quot; &apos; &amp; &#169; &#8710;');
                 return response.json();
             } else {
                 throw new Error("Bad response");
@@ -26,34 +32,64 @@ const QuestionGrid = () => {
 
     const handleQuestion = (e) => {
         e.preventDefault();
-        if(toggleQuestion > -1 || toggleQuestion < 5){
+        target.current.checked = false;
+        if(toggleQuestion > -1 && toggleQuestion < 5){
             changeQuestion(toggleQuestion+1);
+        } else if (toggleQuestion === 5) {
+
         }
     }
-    console.log(questionData);
+
+    const handleChange = (e) => {
+        changeTopic(e.target.value);
+    }
+
+    const choice = (e) => {
+        if(e.target.value === "true"){
+            score.current += 1;
+            console.log(score.current);
+        }
+        target.current = e.target;
+    }
+    console.log(score.current);
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <select name="topic" id="topic">
-                    <option value="Art">Art</option>
-                </select>
-                <input type="submit" value="START" onSubmit={handleSubmit}/>
-            </form>
             <div className="questions">
                 <div>
-                    <p>Questions</p>
                     { questionData ? questionData  === "404" ? (
                 `Invalid`
             ) : (
                 <>
-                <QuestionCard result={questionData.results[toggleQuestion]}/>
+                {toggleQuestion < 5 ? (
+                    <>
+                    <QuestionCard result={questionData.results[toggleQuestion]} choice={choice}/>
                     <form onSubmit={handleQuestion}>
-                        <input type="submit" value="NEXT" onSubmit={handleQuestion} />
+                        <input type="submit" value="NEXT" className="button-53" onSubmit={handleQuestion} />
                     </form>
                 </>
+                ) : (
+                    <>
+                        <p>Score: {(score.current/5)*100}%</p>
+                        <p>Now let's vibe.</p>
+                        <img src={snoop} alt="dancing" />
+                    </>
+                )}
+                </>
             ) : (
-                <p style={{fontSize: "30px"}}>Welcome!</p>
+                <>
+                    <form onSubmit={handleSubmit}>
+                        <select name="topic" id="topic" value={topicData} onChange={handleChange}>
+                            <option value="general">General Knowledge</option>
+                            <option value="animals">Animals</option>
+                            <option value="art">Art</option>
+                            <option value="music">Music</option>
+                            <option value="mythology">Mythology</option>
+                        </select><br/>
+                        <input type="submit" value="START" className="button-53" onSubmit={handleSubmit}/>
+                    </form><br />
+                    <p style={{fontSize: "30px"}}>Welcome!</p>
+                </>
                 )
             }
                 </div>
--- trivia-app/server/index.js
+++ trivia-app/server/index.js
@@ -10,8 +10,56 @@ app.get('/', (req,res) => {
     res.json("Hello from Techtonica");
 });
 
-app.get('/api/trivia/', (req,res) => {
+app.get('/api/trivia/art', (req,res) => {
     const url = "https://opentdb.com/api.php?amount=5&category=25&difficulty=easy&type=multiple";
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        res.send(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/api/trivia/general', (req,res) => {
+    const url = "https://opentdb.com/api.php?amount=5&category=9&type=multiple";
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        res.send(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/api/trivia/animals', (req,res) => {
+    const url = "https://opentdb.com/api.php?amount=5&category=27&type=multiple";
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        res.send(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/api/trivia/music', (req,res) => {
+    const url = "https://opentdb.com/api.php?amount=5&category=12&type=multiple";
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        res.send(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/api/trivia/mythology', (req,res) => {
+    const url = "https://opentdb.com/api.php?amount=5&category=20&type=multiple";
     fetch(url)
     .then((res) => res.json())
     .then((data) => {
