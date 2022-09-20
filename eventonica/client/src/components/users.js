cabf85a992c7e7bb09aa14809e825cc584444fbc
jsmontecillo <jsmontecillo@gmail.com>
2022-09-20 14:42:56 -0700

    updated add and delete forms

--- eventonica/client/src/components/users.js
+++ eventonica/client/src/components/users.js
@@ -1,58 +1,70 @@
 import React, {useState, useEffect} from 'react';
+import DeleteUser from './deleteUser';
 
 const marlin = { name: 'Marlin', email: 'marlin@gmail.com', id: '1' };
 const nemo = { name: 'Nemo', email: 'nemo@gmail.com', id: '2' };
 const dory = { name: 'Dory', email: 'dory@gmail.com', id: '3' };
 
-
-
 const Users = () => {
     const [users, setUsers] = useState([marlin, nemo, dory]);
-    console.log('users', users);
+    const [values, setValues] = useState({name: "", ID: "", email: ""});
 
-const getUsers = () => {
-  fetch('http://localhost:4000/users')
-    .then((res) => res.json())
-    .then((res) => setUsers(res.users));
-};
+    const getUsers = () => {
+        fetch('http://localhost:4000/users')
+        .then((res) => res.json())
+        .then((res) => setUsers(res.users));
+    };
 
-useEffect(() => {
-  // useEffect will run getUsers() every time this component loads, as opposed to just the first time it is rendered.
-  getUsers();
-}, []);
-  return (
-  <section className="user-management">
-    <h2>User Management</h2>
-    <ul id="users-list">
-        {users.map((item, index) => {
-            <li key={index}>{item.name}</li>
-        })}
-        <li>...</li>
-    </ul>
+    useEffect(() => {
+    // useEffect will run getUsers() every time this component loads, as opposed to just the first time it is rendered.
+    getUsers();
+    }, []);
 
-    <div>
-        <h3>Add User</h3>
-        <form id="add-user" action="#">
-            <fieldset>
-                <label>Name</label>
-                <input type="text" id="add-user-name" />
-            </fieldset>
-            {/* Add more form fields here */}
-            <input type="submit" value="Add" />
-        </form>
-    </div>
+    const handleInput = (e) => {
+        setValues((preValues) => ({
+            ...preValues,
+            [e.target.name]: e.target.value
+        }))
+    };
 
-    <div>
-        <h3>Delete User</h3>
-        <form id="delete-user" action="#">
-            <fieldset>
-                <label>User ID</label>
-                <input type="text" id="delete-user-id" />
-             </fieldset>
-            <input type="submit" />
-        </form>
-    </div>
-  </section>
+    const onSubmit = (e) => {
+        e.preventDefault();
+        users.push(values);
+        console.log(values, users);
+        setValues({name: "", ID: "", email: ""});
+    }
+
+    const deleteUser = (deleteId) => {
+        const newUsers = users.filter((i) => i.id !== deleteId);
+        setUsers(newUsers);
+    };
+
+    return (
+    <section className="user-management">
+        <h2>User Management</h2>
+        <ul id="users-list">
+            {users.map((item, index) => {
+                return (<li key={index}>{item.name}</li>)
+            })}
+        </ul>
+
+        <div>
+            <h3>Add User</h3>
+            <form id="add-user" action="#" onSubmit={onSubmit}>
+                <fieldset>
+                    <label>Name</label>
+                    <input type="text" id="add-user-name" name="name" value={values.name} onChange={handleInput}/>
+                    <label>ID</label>
+                    <input type="text" id="add-ID" name="ID" value={values.ID} onChange={handleInput}/>
+                    <label>Email</label>
+                    <input type="text" id="add-email" name="email" value={values.email} onChange={handleInput}/>
+                </fieldset>
+                <input type="submit" value="Add" />
+            </form>
+        </div>
+
+        <DeleteUser delete={deleteUser}/>
+    </section>
   )
 };
 
